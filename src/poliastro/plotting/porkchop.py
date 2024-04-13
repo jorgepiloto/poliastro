8864b4048180872a9e7f6c11a8dbeac30fc541d2
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2024-04-13 18:01:28 +0200

    feat: improve porkchop class

--- src/poliastro/plotting/porkchop.py
+++ src/poliastro/plotting/porkchop.py
@@ -115,9 +115,10 @@ class PorkchopPlotter:
         Time span for launch.
     arrival_span: astropy.time.Time
         Time span for arrival.
-    ax: matplotlib.axes.Axes
-        For custom figures.
-    
+    **maneuver_requirements: dict
+        Requirements for solving the transfer maneuver, including the maximum
+        number of revolutions, desired direction of motion (prograde or
+        retrograde), type of path.
     """
     def __init__(
         self,
@@ -125,194 +126,208 @@ class PorkchopPlotter:
         target_body,
         launch_span,
         arrival_span,
-        ax=None,
+        **maneuver_requirements,
     ):
         self.departure_body = departure_body
         self.target_body = target_body
         self.launch_span = launch_span
         self.arrival_span = arrival_span
-        self.ax = ax
-
-    def plot(
-        self, 
-        plot_c3_lines=True,
-        plot_tof_lines=True,
-        plot_dv_lines=True,
-        plot_av_lines=False,
-        c3_levels=np.linspace(0, 45, 30) * u.km**2 / u.s**2,
-        tof_levels=np.linspace(0, 500, 5) * u.d,
-        dv_levels=np.linspace(0, 5, 5) * u.km / u.s,
-        av_levels=np.linspace(0, 5, 5) * u.km / u.s,
-        title=None,
-    ):
-        """Plots porkchop between two bodies.
-
-        Parameters
-        ----------
-        plot_c3_lines : bool
-            For plotting C3 contour lines.
-        plot_tof_lines : bool
-            For plotting time flight contour lines.
-        plot_dv_lines : bool
-            For plotting departure velocity contour lines.
-        plot_av_lines : bool
-            For plotting arrival velocity contour lines.
-        c3_levels: numpy.ndarray
-            Levels for c3 contour lines.
-        dvl_levels: numpy.ndarray
-            Levels for departure velocity contour lines.
-        avl_levels: numpy.ndarray
-            Levels for arrival velocity contour lines.
-        title : str
-            Title of the plot.
-
-        Returns
-        -------
-        dv_launch: numpy.ndarray
-            Launch delta v
-        dv_arrival: numpy.ndarray
-            Arrival delta v
-        c3_launch: numpy.ndarray
-            Characteristic launch energy
-        c3_arrrival: numpy.ndarray
-            Characteristic arrival energy
-        tof: numpy.ndarray
-            Time of flight for each transfer
-
-        Examples
-        --------
-        >>> from poliastro.plotting.porkchop import PorkchopPlotter
-        >>> from poliastro.bodies import Earth, Mars
-        >>> from poliastro.util import time_range
-        >>> launch_span = time_range("2005-04-30", end="2005-10-07")
-        >>> arrival_span = time_range("2005-11-16", end="2006-12-21")
-        >>> porkchop_plot = PorkchopPlotter(Earth, Mars, launch_span, arrival_span)
-        >>> dv_launch, dev_dpt, c3dpt, c3arr, tof = porkchop_plot.porkchop()
-
-        """
-        self.c3_levels = c3_levels
-        self.tof_levels = tof_levels
-        self.dv_levels = dv_levels
-        self.av_levels = av_levels
-        self.plot_c3_lines = plot_c3_lines
-        self.plot_tof_lines = plot_tof_lines
-        self.plot_dv_lines = plot_dv_lines
-        self.plot_av_lines = plot_av_lines
+        self.maneuver_requirements = maneuver_requirements
 
         # Compute porkchop values
-        dv_launch, dv_arrival, c3_launch, c3_arrival, tof = targetting_vec(
+        self.launch_dv, self.arrival_dv, self.c3_launch, self.c3_arrival, self.tof = targetting_vec(
             self.departure_body,
             self.target_body,
             self.launch_span[np.newaxis, :],
             self.arrival_span[:, np.newaxis],
         )
 
-        # Start drawing porkchop
-        if self.ax is None:
-            fig, self.ax = plt.subplots(figsize=(15, 15))
+        # Compute the minimum c3 energy value for launch and its associated
+        # launch and arrival dates
+        self.min_c3_launch = self.c3_launch.min()
+        self.launch_date_at_min_c3_launch = np.meshgrid(self.launch_span, self.arrival_span)[0][np.unravel_index(self.c3_launch.argmin(), self.c3_launch.shape)]
+        self.arrival_date_at_min_c3_launch = np.meshgrid(self.launch_span, self.arrival_span)[1][np.unravel_index(self.c3_launch.argmin(), self.c3_launch.shape)]
+
+        # Compute the minimum c3 energy value for arrival and its associated
+        # launch and arrival dates
+        self.min_c3_arrival = self.c3_arrival.min()
+        self.launch_date_at_min_c3_arrival = np.meshgrid(self.launch_span, self.arrival_span)[0][np.unravel_index(self.c3_arrival.argmin(), self.c3_arrival.shape)]
+        self.arrival_date_at_min_c3_arrival = np.meshgrid(self.launch_span, self.arrival_span)[1][np.unravel_index(self.c3_arrival.argmin(), self.c3_arrival.shape)]
+
+    def _setup_plot(self, ax):
+        """Setup the plot.
+
+        Parameters
+        ----------
+        ax : matplotlib.axes._subplots.AxesSubplot
+            Axes object for plotting.
+        """
+        if ax is None:
+            self.fig, self.ax = plt.subplots(figsize=(15, 15))
         else:
-            fig = self.ax.figure
-
-        # Draw the contour with colors and the colorbar
-        c3_colors = self.ax.contourf(
-            [D.to_datetime() for D in self.launch_span],
-            [A.to_datetime() for A in self.arrival_span],
-            c3_launch.astype("float64"),
-            c3_levels.astype("float64"),
-        )
-        c3_colorbar = fig.colorbar(c3_colors)
-        c3_colorbar.set_label("$km^2 / s^2$")
-
-        # Draw the solid contour lines on top of previous colors
-        if self.plot_c3_lines:
-            c3_lines = self.ax.contour(
-                [D.to_datetime() for D in self.launch_span],
-                [A.to_datetime() for A in self.arrival_span],
-                c3_launch.astype("float64"),
-                c3_levels.astype("float64"),
-                colors="black",
-                linestyles="solid",
-            )
-            self.ax.clabel(c3_lines, inline=1, fmt="%1.1f", colors="k", fontsize=10)
-
-        # Draw the time of flight lines (if requested)
-        if self.plot_tof_lines:
-            tof_lines = self.ax.contour(
-                [D.to_datetime() for D in self.launch_span],
-                [A.to_datetime() for A in self.arrival_span],
-                (tof / 365.25).astype("float64"),
-                self.tof_levels.to_value(u.year).astype("float64"),
-                colors="red",
-                linestyles="dashed",
-                linewidths=3.5,
-            )
-            #tof_lines.set(path_effects=[patheffects.withStroke(linewidth=6, foreground="k")])
-            tof_lines_labels = self.ax.clabel(
-                tof_lines, inline=1, fmt="%1.0f years", colors="r", fontsize=14, use_clabeltext=True
-            )
-            #plt.setp(tof_lines_labels, path_effects=[
-            #    patheffects.withStroke(linewidth=3, foreground="k")])
-
-        # Draw the departure velocity lines (if requested)
-        if self.plot_dv_lines:
-            dvl_lines = self.ax.contour(
-                [D.to_datetime() for D in self.launch_span],
-                [A.to_datetime() for A in self.arrival_span],
-                dv_launch.astype("float64"),
-                self.dv_levels.astype("float64"),
-                colors="red",
-                linewidths=2.0,
-            )
-            self.ax.clabel(
-                dvl_lines, inline=1, fmt="%1.1f", colors="red", fontsize=12
-            )
-
-        # Draw the arrival velocity lines (if requested)
-        if self.plot_av_lines:
-            avl_lines = self.ax.contour(
-                [D.to_datetime() for D in self.launch_span],
-                [A.to_datetime() for A in self.arrival_span],
-                dv_arrival.astype("float64"),
-                self.av_levels.astype("float64"),
-                colors="white",
-                linewidths=2.0,
-            )
-            avl_lines.set(path_effects=[patheffects.withStroke(linewidth=3, foreground="k")])
-            avl_labels = self.ax.clabel(
-                avl_lines, inline=1, fmt="%1.0f km/s", colors="white",
-                fontsize=14, use_clabeltext=True
-            )
-            plt.setp(avl_labels, path_effects=[
-                patheffects.withStroke(linewidth=3, foreground="k")])
-
-        if title:
-            self.ax.set_title(title, fontsize=14, fontweight="bold")
+            self.ax = ax
+            self.fig = self.ax.figure
 
         self.ax.set_xlabel("Launch date", fontsize=10, fontweight="bold")
         self.ax.set_ylabel("Arrival date", fontsize=10, fontweight="bold")
 
-        # Plot the minimum C3 launch energy point
-        min_c3 = c3_launch.min()        
-        launch_date_at_c3_min = np.meshgrid(self.launch_span, self.arrival_span)[0][np.unravel_index(c3_launch.argmin(), c3_launch.shape)]
-        arrival_date_at_c3_min = np.meshgrid(self.launch_span, self.arrival_span)[1][np.unravel_index(c3_launch.argmin(), c3_launch.shape)]
+    def plot_launch_energy(
+        self,
+        levels=np.linspace(0, 45, 30) * u.km**2 / u.s**2,
+        plot_contour_lines=True,
+        ax=None,
+    ):
+        """Plot the characteristic launch energy.
 
+        Parameters
+        ----------
+        levels: numpy.ndarray
+            Levels for c3 contour lines.
+        ax : matplotlib.axes._subplots.AxesSubplot
+            Axes object for plotting.
 
-        self.ax.plot(
-                launch_date_at_c3_min.to_datetime(),
-                arrival_date_at_c3_min.to_datetime(),
-                "ko",
-                markersize=15,
+        """
+        self._setup_plot(ax)
+
+        c3_launch_colors = self.ax.contourf(
+            [D.to_datetime() for D in self.launch_span],
+            [A.to_datetime() for A in self.arrival_span],
+            self.c3_launch.astype("float64"),
+            levels.astype("float64"),
         )
-        print(f"MINIMUM C3: {min_c3}")
-        print(f"LAUNCH AT: {launch_date_at_c3_min}")
-        print(f"ARRIVAL AT: {arrival_date_at_c3_min}")
+        c3_colorbar = self.fig.colorbar(c3_launch_colors)
+        c3_colorbar.set_label("$km^2 / s^2$")
 
+        # Draw the solid contour lines on top of previous colors
+        if plot_contour_lines:
+            c3_launch_lines = self.ax.contour(
+                [D.to_datetime() for D in self.launch_span],
+                [A.to_datetime() for A in self.arrival_span],
+                self.c3_launch.astype("float64"),
+                levels.astype("float64"),
+                colors="black",
+                linestyles="solid",
+            )
+            self.ax.clabel(c3_launch_lines, inline=1, fmt="%1.1f $km^2/s^2$",
+                           colors="k", fontsize=8)
 
+    def plot_arival_energy(
+        self,
+        levels=np.linspace(0, 45, 30) * u.km**2 / u.s**2,
+        plot_contour_lines=True,
+        ax=None,
+    ):
+        """Plot the characteristic energy at arrival.
 
-        return (
-            dv_launch * u.km / u.s,
-            dv_arrival * u.km / u.s,
-            c3_launch * u.km**2 / u.s**2,
-            c3_arrival * u.km**2 / u.s**2,
-            tof * u.d,
+        Parameters
+        ----------
+        levels: numpy.ndarray
+            Levels for c3 contour lines.
+        ax : matplotlib.axes._subplots.AxesSubplot
+            Axes object for plotting.
+
+        """
+        self._setup_plot(ax)
+
+        c3_arrival_colors = self.ax.contourf(
+            [D.to_datetime() for D in self.launch_span],
+            [A.to_datetime() for A in self.arrival_span],
+            self.c3_launch.astype("float64"),
+            levels.astype("float64"),
         )
+        c3_colorbar = self.fig.colorbar(c3_arrival_colors)
+        c3_colorbar.set_label("$km^2 / s^2$")
+
+        if plot_contour_lines:
+            c3_arrival_lines = self.ax.contour(
+                [D.to_datetime() for D in self.launch_span],
+                [A.to_datetime() for A in self.arrival_span],
+                self.c3_launch.astype("float64"),
+                levels.astype("float64"),
+                colors="black",
+                linestyles="solid",
+            )
+            self.ax.clabel(c3_arrival_lines, inline=1, fmt="%1.1f $km^2/s^2$", colors="k", fontsize=10)
+
+    def plot_time_of_flight(self, levels, ax=None):
+        """Plot the arrival velocity.
+
+        Parameters
+        ----------
+        levels: numpy.ndarray
+            Levels for time of flight contour lines.
+        ax : matplotlib.axes._subplots.AxesSubplot
+            Axes object for plotting.
+
+        """
+        self._setup_plot(ax)
+
+        tof_lines = self.ax.contour(
+            [D.to_datetime() for D in self.launch_span],
+            [A.to_datetime() for A in self.arrival_span],
+            (self.tof / 365.25).astype("float64"),
+            levels.to_value(u.year).astype("float64"),
+            colors="red",
+            linestyles="dashed",
+            linewidths=3.5,
+        )
+        tof_lines.set(path_effects=[patheffects.withStroke(linewidth=6, foreground="k")])
+        tof_lines_labels = self.ax.clabel(
+            tof_lines, inline=True, fmt="%1.0f years", colors="r", fontsize=14, use_clabeltext=True
+        )
+        plt.setp(tof_lines_labels, path_effects=[
+            patheffects.withStroke(linewidth=3, foreground="w")])
+
+    def plot_launch_velocity(self, launch_velocity_levels, ax=None):
+        """Plot the arrival velocity.
+
+        Parameters
+        ----------
+        launch_veocity_levels: numpy.ndarray
+            Levels for launch velocity contour lines.
+        ax : matplotlib.axes._subplots.AxesSubplot
+            Axes object for plotting.
+
+        """
+        self._setup_plot(ax)
+
+        departure_dv_lines = self.ax.contour(
+            [D.to_datetime() for D in self.launch_span],
+            [A.to_datetime() for A in self.arrival_span],
+            self.launch_dv.astype("float64"),
+            launch_velocity_levels.astype("float64"),
+            colors="red",
+            linewidths=2.0,
+        )
+        self.ax.clabel(
+            departure_dv_lines, inline=1, fmt="%1.1f km/s", colors="red", fontsize=12
+        )
+
+    def plot_arrival_velocity(self, arrival_velocity_levels, ax=None):
+        """Plot the arrival velocity.
+
+        Parameters
+        ----------
+        arrival_velocity_levels: numpy.ndarray
+            Levels for arrival velocity contour lines.
+        ax : matplotlib.axes._subplots.AxesSubplot
+            Axes object for plotting.
+
+        """
+        self._setup_plot(ax)
+
+        arrival_dv_lines = self.ax.contour(
+            [D.to_datetime() for D in self.launch_span],
+            [A.to_datetime() for A in self.arrival_span],
+            self.arrival_dv.astype("float64"),
+            arrival_velocity_levels.astype("float64"),
+            colors="red",
+            linewidths=2.0,
+        )
+        self.ax.clabel(
+            arrival_dv_lines, inline=1, fmt="%1.1f km/s", colors="red", fontsize=12
+        )
+
+    def show(self):
+        """Show the porkchop plot."""
+        plt.show()
